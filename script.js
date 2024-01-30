48e0825556ca8a3f2222a2336a0675df62c3e730
Anne Sannella <27774757+Annesannella1@users.noreply.github.com>
2024-01-30 15:30:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas1");
+const canvas = document.getElementById("c");
 const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 900;
@@ -7,102 +7,26 @@ console.log(ctx);
 
 
 
-
-
-class Eye1 {
-      constructor() {
-            this.startX = 120;
-            this.startY = 100;
-            this.width = 200;
-            this.height = 150;
+class Line {
+      constructor(canvas) {
+            this.canvas = canvas;
+            this.startX = Math.random() * this.canvas.width;
+            this.startY = Math.random() * this.canvas.height;
+            this.endX = Math.random() * this.canvas.width;
+            this.endY = Math.random() * this.canvas.height;
+            this.lineWidth = Math.floor(Math.random() * 15 + 1);
+            this.hue = Math.floor(Math.random() * 360);
       }
-      draw() {
-            
-
-           ctx.fillStyle = "hsl(312, 49%, 78%)";
-      
-           ctx.fillRect(this.startX, this.startY, this.width, this.height);
-           ctx.lineWidth = 10;
-           ctx.strokeStyle = "pink";
-           ctx.strokeRect(this.startX, this.startY, this.width, this.height);
+      draw(context){
+            context.strokeStyle = "hsl(" + this.hue + ", 100%, 50%);
+            context.lineWidth = this.lineWidth;
+            context.beginPath();
+            context.moveTo(this.startX, this.startY);
+            contect.lineTo(this.endX, this.endY);
+            context.stroke();
       }
 }     
-
-class Eye2 {
-      constructor() {
-            this.startX = 400;
-            this.startY = 100;
-            this.width = 200;
-            this.height = 150;
-      }
-      draw() {
             
-
-           ctx.fillStyle = "hsl(312, 49%, 78%)";
-      
-           ctx.fillRect(this.startX, this.startY, this.width, this.height);
-           ctx.lineWidth = 10;
-           ctx.strokeStyle = "pink";
-           ctx.strokeRect(this.startX, this.startY, this.width, this.height);
-      }
-}  
-
- class Line1 {
-       constructor() {
-             this.x1 = 350;
-             this.y1 = 350;
-             this.x2 = 400;
-             this.y2 = 700;
-             this.x3 = 300;
-             this.y3 = 700;
-       }
-       draw() {
-            ctx.beginPath();
-            ctx.moveTo(this.x1, this.y1);
-            ctx.lineTo(this.x2, this.y2);
-            ctx.lineTo(this.x3, this.y3);
-            ctx.lineTo(this.x1, this.y1);
-            ctx.strokeStyle = "hsl(82, 49%, 62%)";
-            ctx.closePath();
-            ctx.stroke();
-             
-      
-       }
- }      
-
- class Text {
-       constructor() {
-             this.showWord = "Who Am I?"
-             this.x1 = 230;
-             this.y1 = 800;
-             this.fillColor = "hsl(312, 49%, 78%)";
-             this.fontSize = "50px";
-       }
-       draw() {
-            
-             ctx.font = `${this.fontSize} Georgia`;
-             ctx.fillStyle = this.fillColor;
-             ctx.fillText(this.showWord, this.x1, this.y1);
-      
-             
-       }
- }      
-
-
-
-//ctx.beginPath();
-//ctx.moveTo(350, 350);
-//ctx.lineTo(400, 700);
-//ctx.lineTo(300, 700);
-//ctx.lineTo(350, 350);
-//ctx.strokeStyle = "hsl(320, 100%, 50%)";
-//ctx.stroke();
-
-const rect1 = new Eye1();
+const rect1 = new Line();
 rect1.draw();       
-const rect2 = new Eye2();
-rect2.draw();       
-const line1 = new Line1();
-line1.draw();
-const text1 = new Text();
-text1.draw();
+
